e0172ff8c721391754a1117dee26125fa74f3913
MathieuGAY21 <m.gay@standardenligne.fr>
2024-05-13 15:06:07 +0200

    add React router + useNavigate

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { TabMenu } from 'primereact/tabmenu';
+import { Button } from 'primereact/button';
+import { useNavigate } from 'react-router-dom';
+
+export default function NavBar() {
+    const navigate = useNavigate();
+
+    const items = [
+        { label: 'Accueil', icon: 'pi pi-home', command: () => navigate('/') },
+        { label: 'Connexion', icon: 'pi pi-user', command: () => navigate('/login') },
+        { label: 'Evenement', icon: 'pi pi-tag', command: () => navigate('/evenement') },
+    ];
+
+    // Fonction pour déconnecter l'utilisateur et supprimer le cookie
+    const handleDeconnexion = () => {
+        // Supprimer le cookie en définissant sa date d'expiration à une date passée
+        document.cookie = "JWSToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        // Rediriger l'utilisateur vers la page de connexion ou effectuer d'autres actions nécessaires
+    };
+
+    // Vérifier s'il y a un cookie JWSToken
+    const cookieExists = document.cookie.includes('JWSToken');
+
+    return (
+        <div className="flex flex-row gap-6">
+            <TabMenu model={items} />
+            {cookieExists && <Button icon='pi pi-sign-out' label='Déconnexion' onClick={handleDeconnexion} />}
+        </div>
+    );
+}
