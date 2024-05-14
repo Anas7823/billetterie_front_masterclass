452e2e20545522cd99009c38dd7b36c2055e5a7a
Anas7823 <anaselkhiat78@gmail.com>
2024-05-14 16:18:58 +0200

    creation de pdf pour chaque reservation

--- src/pages/MonCompte.jsx
+++ src/pages/MonCompte.jsx
@@ -1,114 +1,126 @@
+import * as React from "react";
+import { useState, useEffect } from 'react';
 import { Button } from 'primereact/button';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
+import { Page, Text, View, Document, StyleSheet, PDFDownloadLink } from '@react-pdf/renderer';
+
 import '../assets/css/user/monCompte.css';
-import * as React from "react";
-import { useState, useEffect } from 'react';
-import axios from 'axios';
 import usersData from '../jeuDeDonnes/users.json';
 import eventsData from '../jeuDeDonnes/events.json';
 import reservationsData from '../jeuDeDonnes/reservations.json';
 
 function MonCompte() {
     const [users, setUsers] = useState([]);
-
     const [events, setEvents] = useState([]);
-
     const [reservations, setReservations] = useState([]);
 
-    // fonction pour les jeux de données | A supprimer plus tard
     useEffect(() => {
         setUsers(usersData);
-    }, []);
-
-    useEffect(() => {
         setEvents(eventsData);
-    }, []);
-
-    useEffect(() => {
         setReservations(reservationsData);
     }, []);
 
-    // Récupérer les informations de l'utilisateur connecté depuis le localStorage
     const userId = parseInt(localStorage.getItem('userId'), 10);
-
-    // Récupérer les informations de l'utilisateur connecté depuis le jeu de données
     const user = users.find(user => user.id === userId);
+    const reservationsUser = reservations.filter(reservation => reservation.id_user === userId);
+    const eventsUser = reservationsUser.map(reservation => events.find(event => event.id_evenement === reservation.id_evenement));
 
-    let reservationsUser = reservations.filter(reservation => reservation.id_user === userId); // Récupérer les réservations de l'utilisateur connecté
+    const ReservationDocument = ({ reservation, event }) => (
+        <Document>
+            <Page size="A4" style={styles.page}>
+                <View style={styles.section}>
+                    <Text>Réservation numéro: {reservation.UUID}</Text>
+                </View>
+                <View style={styles.section}>
+                    <Text>{user.nom} {user.prenom}</Text>
+                    <Text>Email: {user.mail}</Text>
+                    <Text>Âge: {user.age} ans</Text>
+                </View>
+                <View style={styles.section}>
+                    <Text>Date de réservation: {reservation.date_reservation}</Text>
+                    <Text>Nombre de personnes: {reservation.nb_personne}</Text>
+                    <Text>Événement: {event.nom}</Text>
+                    <Text>Date de début: {event.date_debut}</Text>
+                    <Text>Date de fin: {event.date_fin}</Text>
+                </View>
+            </Page>
+        </Document>
+    );
 
-    let eventsUser = reservationsUser.map(reservation => events.find(event => event.id_evenement === reservation.id_evenement)); // Récupérer les événements correspondant aux réservations de l'utilisateur connecté
+    const styles = StyleSheet.create({
+        page: {
+            flexDirection: 'column',
+            backgroundColor: '#E4E4E4',
+            padding: 20
+        },
+        section: {
+            margin: 10,
+            padding: 10,
+            flexGrow: 0.25 // sert à définir la taille de la section
+        }
+    });
 
     return (
         <div className="flex flex-column align-items-center justify-content-center inscription">
-
             <div className="monCompte">
                 <div className="header_profil">
                     <img src="https://institutcommotions.com/wp-content/uploads/2018/05/blank-profile-picture-973460_960_720-1.png" alt="" className='img_pp' />
                     {user ? (
-                        <h2>
-                            {user.nom} {user.prenom}
-                        </h2>
+                        <h2>{user.nom} {user.prenom}</h2>
                     ) : (
                         <h2>Utilisateur non trouvé</h2>
                     )}
-
                 </div>
-                {user ? (
+                {user && (
                     <>
-                    <div className="infos">
-                        <h3>Informations</h3>
-                        <p>Âge: {user.age} ans</p>
-                        <p>Email: {user.mail}</p>
-                        <p>Rôle: {user.role} </p>
-                    </div>
-
-                    <div className="historique_reservations">
-                        <h3>Historique des réservations</h3>
-                        {eventsUser[0] ? (
-                            <>
-                            <p>Nombre total de réservations: {eventsUser.length}</p>
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th>Evenement</th>
-                                        <th>Date</th>
-                                        <th>Nombre de places</th>
-                                        <th>Actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {eventsUser.map((event, index) => (
-                                        <tr key={index}>
-                                            <td>{event.nom}</td>
-                                            <td>{event.date_debut} - {event.date_fin}</td>
-                                            <td>{reservationsUser[index].nb_personne}</td>
-                                            <td>
-                                                <Button label="Télécharger un PDF" className="p-button-contrast" icon="pi pi-download" />
-                                            </td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        </>
-                        ) : (
-                        <>
-
-                        </>
-                        )}
-
-                        
-                    </div>
+                        <div className="infos">
+                            <h3>Informations</h3>
+                            <p>Âge: {user.age} ans</p>
+                            <p>Email: {user.mail}</p>
+                            <p>Rôle: {user.role}</p>
+                        </div>
+                        <div className="historique_reservations">
+                            <h3>Historique des réservations</h3>
+                            {eventsUser.length > 0 ? (
+                                <>
+                                    <p>Nombre total de réservations: {eventsUser.length}</p>
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th>Événement</th>
+                                                <th>Date</th>
+                                                <th>Nombre de places</th>
+                                                <th>Actions</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {eventsUser.map((event, index) => (
+                                                <tr key={index}>
+                                                    <td>{event.nom}</td>
+                                                    <td>{event.date_debut} - {event.date_fin}</td>
+                                                    <td>{reservationsUser[index].nb_personne}</td>
+                                                    <td>
+                                                        <PDFDownloadLink
+                                                            document={<ReservationDocument reservation={reservationsUser[index]} event={event} />}
+                                                            fileName={`reservation_${reservationsUser[index].UUID}.pdf`}
+                                                        >
+                                                            {({ loading }) => loading ? 'Chargement...' : <Button label="Télécharger un PDF" className="p-button-contrast" icon="pi pi-download" />}
+                                                        </PDFDownloadLink>
+                                                    </td>
+                                                </tr>
+                                            ))}
+                                        </tbody>
+                                    </table>
+                                </>
+                            ) : (
+                                <p>Aucune réservation trouvée.</p>
+                            )}
+                        </div>
                     </>
-                    )
-                    : (
-                        <h2>Utilisateur non trouvé</h2>
-                    )}
-
+                )}
             </div>
-
-            
         </div>
     );
 }
