0bff98f1ad3dacf9bc76fef1768c1260d4968c84
MathieuGAY21 <m.gay@standardenligne.fr>
2024-05-15 16:55:45 +0200

    fix page calendrier | affichage des événements à venir seulement

--- src/pages/Calendrier.jsx
+++ src/pages/Calendrier.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import '../assets/css/Calendrier/calendrier.css'
-import evenements from '../jeuDeDonnes/events.json'; // Assure-toi que le chemin d'accès soit correct
+import '../assets/css/Calendrier/calendrier.css';
+import evenements from '../jeuDeDonnes/events.json'; 
 
 export default function Calendrier() {
+    // Filtrer les événements à venir
+    const evenementsAVenir = evenements.filter((evenement) => new Date(evenement.date_debut) > new Date());
+
     return (
         <div className='flex flex-column justify-content-center align-items-center mt-6'>
             <h1>Calendrier des événements</h1>
-            <div className='flex flex-row gap-8 mt-5'>
-                {evenements.map((evenement) => (
+            <div className='flex flex-wrap flex-row justify-content-center gap-8 mt-5'>
+                {evenementsAVenir.map((evenement) => (
                     <div className='flex flex-column gap-3 calendar-container' key={evenement.id_evenement}>
                         <i style={{fontSize:'30px'}} className="pi pi-calendar"></i>
                         <div><strong style={{fontSize:'20px'}}>| {evenement.nom}</strong></div>
