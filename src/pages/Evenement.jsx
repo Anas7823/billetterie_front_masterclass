8defec72b52b7eae61d0ae8f0b490305d9832b25
Anas7823 <anaselkhiat78@gmail.com>
2024-05-15 11:11:00 +0200

    calcul des places restantes + form de reservation pour les clients connecté si places restantes et event actif

--- src/pages/Evenement.jsx
+++ src/pages/Evenement.jsx
@@ -1,11 +1,29 @@
+import { Button } from 'primereact/button';
+import { Dialog } from 'primereact/dialog';
+import { InputText } from 'primereact/inputtext';
+
 import React, { useState, useEffect } from 'react';
 import '../assets/css/Evenement/evenement.css'
 import { Tag } from 'primereact/tag';
 import evenementsData from '../jeuDeDonnes/events.json';
+import reservationData from '../jeuDeDonnes/reservations.json';
+import usersData from '../jeuDeDonnes/users.json';
 
 export default function Evenement() {
     const [evenements, setEvenements] = useState([]);
     const [selectedEvent, setSelectedEvent] = useState(null);
+    const [user, setUser] = useState(null);
+    const [userConnected, setUserConnected] = useState(false);
+
+    useEffect(() => {
+        const userId = parseInt(localStorage.getItem('userId'), 10);
+        const user = usersData.find(user => user.id === userId);
+        if (user) {
+            const userIsConnected = localStorage.getItem("isConnected");
+            setUser(user);
+            setUserConnected(userIsConnected);
+        }
+    }, []);
 
     useEffect(() => {
         setEvenements(evenementsData);
@@ -16,11 +34,27 @@ export default function Evenement() {
     };
 
     const handleEventClick = (event) => {
-        if (selectedEvent && selectedEvent.id === event.id) {
+        if (selectedEvent && selectedEvent.id_evenement === event.id_evenement) {
             setSelectedEvent(null);
         } else {
             setSelectedEvent(event);
         }
+    };
+
+    const placesRestantes = (evenement) => {
+        const reservations = reservationData.filter(reservation => reservation.id_evenement === evenement.id_evenement);
+        return evenement.nb_places - reservations.reduce((acc, reservation) => acc + reservation.nb_personne, 0);
+    }
+
+    // Modal de réservation
+    const [visible, setVisible] = useState(false);
+
+    const showDialog = () => {
+        setVisible(true);
+    };
+
+    const hideDialog = () => {
+        setVisible(false);
     };
 
     return (
@@ -35,8 +69,24 @@ export default function Evenement() {
                                 <p><span className="font-semibold">Type:</span> {evenement.type}</p>
                                 <p><span className="font-semibold">Date de début:</span> {new Date(evenement.date_debut).toLocaleString()}</p>
                                 <p><span className="font-semibold">Date de fin:</span> {new Date(evenement.date_fin).toLocaleString()}</p>
+                                <p><span className="font-semibold">Nombre de places: </span> {evenement.nb_places}</p>
+                                <p><span className="font-semibold">Places restantes: </span> {placesRestantes(evenement)}</p>
                                 <p><span className="font-semibold">Description:</span> {evenement.description}</p>
-                                <p><span className="font-semibold">État:</span> {statusBadgeTemplate(evenement.etat)}</p>
+                                <p><span className="font-semibold">État:</span> {statusBadgeTemplate(evenement.etat === 'valide' ? "ouvert" : "annulé")}</p>
+                                {userConnected && evenement.etat === 'valide' && placesRestantes(evenement) > 0 && (
+                                    <>
+                                        <Button label="Réserver" className="p-button-help" onClick={(e) => { e.stopPropagation(); showDialog(); }} />
+                                        <Dialog header={evenement.nom} visible={visible} style={{ width: '50vw' }} onHide={hideDialog}>
+                                            <form action="" onSubmit={(e) => { e.preventDefault(); hideDialog(); }} className="flex flex-column gap-2">
+                                                <label htmlFor="nbPersonne">Nombre de personnes</label>
+                                                <InputText id="nbPersonne" type="number" />
+                                                <label htmlFor="dateReservation">Date de réservation</label>
+                                                <InputText id="dateReservation" type="date" />
+                                                <Button label="Réserver" className="p-button-help" type='submit' />
+                                            </form>
+                                        </Dialog>
+                                    </>
+                                )}
                             </div>
                         )}
                     </div>
